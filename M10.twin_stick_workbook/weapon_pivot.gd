2f39441c72aa61833eb46b8638230ccd9980c8c4
raweggyolk <edtso@s.sfusd.edu>
2025-01-15 11:16:11 -0800

    Weapon Pivot

---
+++ M10.twin_stick_workbook/weapon_pivot.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var _weapon_anchor: Marker2D = $WeaponAnchor
+
+func _process(_delta: float) -> void:
+	var aim_direction := Vector2.ZERO
+	aim_direction = global_position.direction_to(get_global_mouse_position())
+	if aim_direction.length() > 0.1:
+		rotation = aim_direction.angle()
+	
+	z_index = 3
+	if aim_direction.y < 0.0:
+		z_index = 1
