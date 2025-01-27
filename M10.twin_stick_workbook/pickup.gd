da503cc9b8cc68057d04238a48597fe2e32ae329
raweggyolk <edtso@s.sfusd.edu>
2025-01-27 14:24:56 -0800

    .

--- M10.twin_stick_workbook/pickup.gd
+++ M10.twin_stick_workbook/pickup.gd
@@ -3,7 +3,7 @@ class_name Pickup extends Area2D
 
 @export var item: Item = null: set = set_item
 @onready var _sprite_2d: Sprite2D = %Sprite2D
-@onready var _audio_stream_player: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
 
 func _ready() -> void:
 	set_item(item)
@@ -11,15 +11,14 @@ func _ready() -> void:
 		if body is Player:
 			item.use(body)
 			set_deferred("monitoring", false)
-			_audio_stream_player.play()
+			audio_stream_player_2d.play()
 			queue_free()
 	)
 	
-
 func set_item(value: Item) -> void:
 	item = value
 	if _sprite_2d != null:
 		_sprite_2d.texture = item.texture
-	if _audio_stream_player != null:
-		_audio_stream_player.stream = item.sound_on_pickup
+	if audio_stream_player_2d != null:
+		audio_stream_player_2d.stream = item.sound_on_pickup
 		
