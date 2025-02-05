2b644a993902083dd675b5c9855f5df0718a5957
raweggyolk <edtso@s.sfusd.edu>
2025-02-05 11:13:36 -0800

    Chest and Pickup Audio

--- M10.twin_stick_workbook/pickup.gd
+++ M10.twin_stick_workbook/pickup.gd
@@ -4,15 +4,19 @@ class_name Pickup extends Area2D
 @export var item: Item = null: set = set_item
 @onready var _sprite_2d: Sprite2D = %Sprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var _animation_player: AnimationPlayer = %AnimationPlayer
 
 func _ready() -> void:
 	set_item(item)
 	body_entered.connect(func (body: Node2D) -> void:
 		if body is Player:
+			_animation_player.play("health")
 			item.use(body)
 			set_deferred("monitoring", false)
 			audio_stream_player_2d.play()
-			queue_free()
+			_animation_player.animation_finished.connect(func (_animation_name: String) -> void:
+				queue_free()
+			)
 	)
 	
 func set_item(value: Item) -> void:
